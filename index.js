96fbcf1fd4107825a12a51fb9be6dc08a0a9a115
Neven Bedr <neven2225@gmail.com>
2020-06-12 11:15:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code here
 class Dog {
-  constructor(name, breed) {
-    this.name = name;
-    this.breed = breed;
+  constructor(food,drink )
+    this.food = food;
+    this.drink=drink;
   }
 }
 class Breakfast{
